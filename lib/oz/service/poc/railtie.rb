1e3c8822c79124be213bce96d1261f6860135463
madeleinvanniekerk <madelein@offerzen.com>
2021-09-01 11:46:56 +0200

    remove dd config

--- lib/oz/service/poc/railtie.rb
+++ lib/oz/service/poc/railtie.rb
@@ -15,24 +15,24 @@ class OzServicePocRailtie < Rails::Railtie
       # Log to a dedicated file
       config.lograge.logger = ActiveSupport::Logger.new(File.join(Rails.root, 'log', "#{Rails.env}.log"))
 
-      config.lograge.custom_options = lambda do |event|
-        # Retrieves trace information for current thread
-        correlation = Datadog.tracer.active_correlation
-
-        {
-          # Adds IDs as tags to log output
-          :dd => {
-            # To preserve precision during JSON serialization, use strings for large numbers
-            :trace_id => correlation.trace_id.to_s,
-            :span_id => correlation.span_id.to_s,
-            :env => correlation.env.to_s,
-            :service => correlation.service.to_s,
-            :version => correlation.version.to_s
-          },
-          :ddsource => ["ruby"],
-          :params => event.payload[:params].reject { |k| %w(controller action).include? k }
-        }
-      end
+      # config.lograge.custom_options = lambda do |event|
+      #   # Retrieves trace information for current thread
+      #   correlation = Datadog.tracer.active_correlation
+      #
+      #   {
+      #     # Adds IDs as tags to log output
+      #     :dd => {
+      #       # To preserve precision during JSON serialization, use strings for large numbers
+      #       :trace_id => correlation.trace_id.to_s,
+      #       :span_id => correlation.span_id.to_s,
+      #       :env => correlation.env.to_s,
+      #       :service => correlation.service.to_s,
+      #       :version => correlation.version.to_s
+      #     },
+      #     :ddsource => ["ruby"],
+      #     :params => event.payload[:params].reject { |k| %w(controller action).include? k }
+      #   }
+      # end
     end
 
     puts Rails.application.config.lograge
