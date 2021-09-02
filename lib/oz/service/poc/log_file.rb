61ef3292c623a505127baf8c942d18c53328ca94
madeleinvanniekerk <madelein@offerzen.com>
2021-09-02 14:13:08 +0200

    remove file

--- lib/oz/service/poc/log_file.rb
+++ lib/oz/service/poc/log_file.rb
@@ -2,17 +2,19 @@ module Oz
   module Service
     module Poc
       class LogFile
-        log_file_name = "./datadog/conf.d/ruby.yaml"
+        log_file_name = "./datadog/conf.d/ruby.yaml".freeze
         file_contents = """
 logs:
   - type: file
   path: '/app/log/production.log'
   service: ruby
   source: ruby
-  sourcecategory: sourcecode"""
+  sourcecategory: sourcecode""".freeze
+
         unless File.exist?(File.dirname(log_file_name))
           FileUtils.mkdir_p(File.dirname(log_file_name))
         end
+
         File.open(log_file_name, "w") do |f|
           f.write file_contents
         end
