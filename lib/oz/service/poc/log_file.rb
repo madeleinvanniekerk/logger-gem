2553f00e6ddb8fe5780aada6a09caeed348ec7b3
madeleinvanniekerk <madelein@offerzen.com>
2021-09-02 13:53:38 +0200

    write file

---
+++ lib/oz/service/poc/log_file.rb
@@ -0,0 +1,22 @@
+module Oz
+  module Service
+    module Poc
+      class LogFile
+        log_file_name = "./datadog/conf.d/ruby.yaml"
+        file_contents = """
+logs:
+  - type: file
+  path: '/app/log/production.log'
+  service: ruby
+  source: ruby
+  sourcecategory: sourcecode"""
+        unless File.exist?(File.dirname(log_file_name))
+          FileUtils.mkdir_p(File.dirname(log_file_name))
+        end
+        File.open(log_file_name, "w") do |f|
+          f.write file_contents
+        end
+      end
+    end
+  end
+end
